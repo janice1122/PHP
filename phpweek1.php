be8509414afbe1276091b64642c43f6d9134b2f8
陳姮均 <113579383+janice1122@users.noreply.github.com>
2023-02-20 22:22:59 +0800

    Add files via upload

---
+++ phpweek1.php
@@ -0,0 +1,16 @@
+<HTML>
+
+<HEAD>
+    <meta charset="UFT-8">
+    <title>PHP WEEK1</title>
+</HEAD>
+<body>
+    <?php
+        echo "hello php"
+        $x=5;
+        $y=3;
+        $i=$x*$y;
+        echo "<br/>"
+        echo "5*3="+$i;
+</body>
+</HTML>
