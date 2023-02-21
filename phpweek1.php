27790d15a42c14330164580867e844c57c379cca
陳姮均 <113579383+janice1122@users.noreply.github.com>
2023-02-21 21:50:27 +0800

    Update phpweek1.php

--- phpweek1.php
+++ phpweek1.php
@@ -6,12 +6,12 @@
 </HEAD>
 <body>
     <?php
-        echo "hello php"
+        echo "hello php";
         $x=5;
         $y=3;
         $i=$x*$y;
-        echo "<br/>"
-        echo "5*3="+$i;
+        echo "<br/>";
+        echo "5*3=$i";
     ?>
 </body>
 </HTML>
